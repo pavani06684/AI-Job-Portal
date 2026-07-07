6b4463022b787db1fc963f53c6f2425877a9afcc
Pavani Chelluri <pavani06072004@gmail.com>
2026-07-07 18:37:44 +0530

    Configure project for Render deployment

--- job_portal/settings.py
+++ job_portal/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,12 +21,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!t(x$@ve=^==e!+cb3_%9mome1c4(ep(lzjx=36o4qu1p05ut-'
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    'django-insecure-!t(x$@ve=^==e!+cb3_%9mome1c4(ep(lzjx=36o4qu1p05ut-'
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG","False")=="True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    ".onrender.com",
+]
 
 
 # Application definition
@@ -45,6 +53,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,6 +128,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIR=[
+    BASE_DIR/"static",
+]
+STATIC_ROOT=BASE_DIR/"staticfiles"
+STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 
 AUTH_USER_MODEL='accounts.User'
 
@@ -129,7 +144,4 @@ LOGOUT_REDIRECT_URL="home"
 MEDIA_URL="/media/"
 MEDIA_ROOT=BASE_DIR/"media"
 
-STATIC_URL="static/"
-STATICFILES_DIR=[
-    BASE_DIR/"static",
-]
+
